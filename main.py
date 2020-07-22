e2024a548cbbe90bca0ade79347d4cbe63c9849a
FreekillRS <freekill246@gmail.com>
2020-07-22 18:12:40 +0200

    Optimized scanning algorithm
    Average solve: 0.019s

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+
 import time
 
 start_time = time.time()
@@ -35,91 +37,71 @@ def removeUnneeded(x, y):
 
 	for u in range(x // 3 * 3, x // 3 * 3 + 3):
 		for p in range(y // 3 * 3, y // 3 * 3 + 3):
-			if sudoku[x][y] in duSudoku[q][p]:
-				duSudoku[q][p].remove(sudoku[x][y])
+			if sudoku[x][y] in duSudoku[u][p]:
+				duSudoku[u][p].remove(sudoku[x][y])
 
 
-hundred = 500
-while hundred:
-	sudoku = [[0, 0, 0, 8, 0, 1, 0, 0, 0],
-	          [0, 0, 0, 0, 0, 0, 0, 4, 3],
-	          [5, 0, 0, 0, 0, 0, 0, 0, 0],
-	          [0, 0, 0, 0, 7, 0, 8, 0, 0],
-	          [0, 0, 0, 0, 0, 0, 1, 0, 0],
-	          [0, 2, 0, 0, 3, 0, 0, 0, 0],
-	          [6, 0, 0, 0, 0, 0, 0, 7, 5],
-	          [0, 0, 3, 4, 0, 0, 0, 0, 0],
-	          [0, 0, 0, 2, 0, 0, 6, 0, 0]
-	          ]
+sudoku = [[0, 0, 0, 8, 0, 1, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 4, 3],
+          [5, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 7, 0, 8, 0, 0],
+          [0, 0, 0, 0, 0, 0, 1, 0, 0],
+          [0, 2, 0, 0, 3, 0, 0, 0, 0],
+          [6, 0, 0, 0, 0, 0, 0, 7, 5],
+          [0, 0, 3, 4, 0, 0, 0, 0, 0],
+          [0, 0, 0, 2, 0, 0, 6, 0, 0]
+          ]
 
-	for t in sudoku:
-		print(t)
-	print("\n")
+duSudoku = [[set(), set(), set(), set(), set(), set(), set(), set(), set()],
+            [set(), set(), set(), set(), set(), set(), set(), set(), set()],
+            [set(), set(), set(), set(), set(), set(), set(), set(), set()],
+            [set(), set(), set(), set(), set(), set(), set(), set(), set()],
+            [set(), set(), set(), set(), set(), set(), set(), set(), set()],
+            [set(), set(), set(), set(), set(), set(), set(), set(), set()],
+            [set(), set(), set(), set(), set(), set(), set(), set(), set()],
+            [set(), set(), set(), set(), set(), set(), set(), set(), set()],
+            [set(), set(), set(), set(), set(), set(), set(), set(), set()],
+            ]
 
-	hundred -= 1
-	duSudoku = [[set(), set(), set(), set(), set(), set(), set(), set(), set()],
-	            [set(), set(), set(), set(), set(), set(), set(), set(), set()],
-	            [set(), set(), set(), set(), set(), set(), set(), set(), set()],
-	            [set(), set(), set(), set(), set(), set(), set(), set(), set()],
-	            [set(), set(), set(), set(), set(), set(), set(), set(), set()],
-	            [set(), set(), set(), set(), set(), set(), set(), set(), set()],
-	            [set(), set(), set(), set(), set(), set(), set(), set(), set()],
-	            [set(), set(), set(), set(), set(), set(), set(), set(), set()],
-	            [set(), set(), set(), set(), set(), set(), set(), set(), set()],
-	            ]
+root = Tk()
 
+for i in range(9):
+	for j in range(9):
+		if sudoku[i][j] == 0:
+			duSudoku[i][j] = potentialCandidate(i, j)
+
+unchanged = 1
+while unchanged:
+	unchanged = 0
 	for i in range(9):
 		for j in range(9):
 			if sudoku[i][j] == 0:
-				duSudoku[i][j] = potentialCandidate(i, j)
+				potentialX = set(duSudoku[i][j])
+				potentialY = set(duSudoku[i][j])
+				potentialCell = set(duSudoku[i][j])
 
-	unchanged = 1
-	while unchanged:
-		unchanged = 0
-		for i in range(9):
-			for j in range(9):
-				if sudoku[i][j] == 0:
-					potentialX = set(duSudoku[i][j])
-					potentialY = set(duSudoku[i][j])
-					potentialCell = set(duSudoku[i][j])
+				for m in range(9):
+					if m != j:
+						potentialX = potentialX.difference(duSudoku[i][m])
+					if m != i:
+						potentialY = potentialY.difference(duSudoku[m][j])
 
-					for m in range(9):
-						if m != j:
-							potentialX = potentialX.difference(duSudoku[i][m])
-						if m != i:
-							potentialY = potentialY.difference(duSudoku[m][j])
+				for q in range(i // 3 * 3, i // 3 * 3 + 3):
+					for r in range(j // 3 * 3, j // 3 * 3 + 3):
+						if q != i or r != j:
+							potentialCell = potentialCell.difference(duSudoku[q][r])
+				potentialX = potentialX.union(potentialY.union(potentialCell))
 
-					if len(potentialX) == 1:
-						sudoku[i][j] = potentialX.pop()
-						removeUnneeded(i, j)
-						duSudoku[i][j] = set()
-						unchanged = 1
-						break
+				if len(potentialX) == 1:
+					sudoku[i][j] = potentialX.pop()
+					removeUnneeded(i, j)
+					duSudoku[i][j].clear()
+					unchanged = 1
+					break
 
-					if len(potentialY) == 1:
-						sudoku[i][j] = potentialY.pop()
-						removeUnneeded(i, j)
-						duSudoku[i][j] = set()
-						unchanged = 1
-						break
-
-					for q in range(i // 3 * 3, i // 3 * 3 + 3):
-						for r in range(j // 3 * 3, j // 3 * 3 + 3):
-							if q != i or r != j:
-								potentialCell = potentialCell.difference(duSudoku[q][r])
-
-					if len(potentialCell) == 1:
-						sudoku[i][j] = potentialCell.pop()
-						removeUnneeded(i, j)
-						duSudoku[i][j] = set()
-						unchanged = 1
-						break
-
-		for i in range(9):
-			for j in range(9):
 				count = 0
 				for k in range(9):
-					if duSudoku[i][j] == duSudoku[i][k]:
+					if k >= i and duSudoku[i][j] == duSudoku[i][k]:
 						count += 1
 						if count == len(duSudoku[i][j]):
 							unchanged = 1
@@ -129,7 +111,7 @@ while hundred:
 
 				count = 0
 				for k in range(9):
-					if duSudoku[i][j] == duSudoku[k][j]:
+					if k >= j and duSudoku[i][j] == duSudoku[k][j]:
 						count += 1
 						if count == len(duSudoku[i][j]):
 							unchanged = 1
@@ -151,5 +133,3 @@ while hundred:
 
 for t in sudoku:
 	print(t)
-
-print("--- %s seconds ---" % ((time.time() - start_time) / 500))
